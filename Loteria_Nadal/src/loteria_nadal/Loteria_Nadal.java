184afffb84c7f1e00aeb275244c1011f2ae1f6f1
ausias <ausias@hINFO6-8.xiam.cat>
2023-01-10 12:10:02 +0100

    p1

--- Loteria_Nadal/src/loteria_nadal/Loteria_Nadal.java
+++ Loteria_Nadal/src/loteria_nadal/Loteria_Nadal.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package loteria_nadal;
 
-/**
- *
- * @author ausias
- */
+import java.util.*;
+
 public class Loteria_Nadal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //blablalbalba
-        //dasasasdf
+        
+        
+        
     }
     
 }
